9a8b021e8563d784109331cbc5f92bdcd34e925b
Aaron Limbu <aaronlimbu381@gmail.com>
2025-06-09 23:27:29 +0545

    code

--- scanner/fuzz.go
+++ scanner/fuzz.go
@@ -1,26 +1,47 @@
 package main
+
 import (
-	"buffio"
 	"fmt"
+	"os"
 	"net/http"
 	"flag"
-	"os"
 	"strings"
+	"bufio"
 )
 
 var (
 	url string 
-	path string
+	path string 
 )
 
-func fuzzWeb(target string, wpath string){
-	
+func fuzzPath(target, wpath string){
+	file, err := os.Open(wpath)
+	if err != nil{
+		fmt.Println("[-] Failed to open wordlists ",err)
+		return 
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		path := scanner.Text()
+		fullUrl := strings.TrimRight(target,"/") + "/" + wpath
+		resp, err := http.Get(fullUrl)
+		if err != nil {
+			continue
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode == 200{
+			fmt.Printf("[+] Found: %s (200 OK)\n",fullUrl)
+		}
+	}
 }
 
-
 func main(){
-	flag.StringVar(&url,"url","default","example:- -url https://example.com")
-	flag.StringVar(&path,"path","../wordlists/wordlists.txt","example:- -path ../wordlists/wordlists.txt or /home/username/Wordlists/wordlists.txt")
+	flag.StringVar(&url,"url","default","example: -url https://example.com")
+	flag.StringVar(&path,"path","default")
 	flag.Parse()
-
+	if url == ""{
+		fmt.Println("[i] Enter url ")
+	}
+	fuzzPath(url, path)
 }
