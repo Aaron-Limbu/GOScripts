bc6de1522ee22f30ed45c17e80154d64ba667cbf
Aaron Limbu <aaronlimbu381@gmail.com>
2025-06-10 23:56:52 +0545

    code

---
+++ reports/reports.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"os"
+)
+
+type ScanRepo struct {
+	Module string 
+	Target string
+	Details map[string]string
+	Status string						
+}
+
+var Results []ScanRepo
+
+func AddResult(module, target string, details map[string]string, status string){
+	result := ScanRepo{
+		Module: module,
+		Target: target,
+		Details: details,
+		Status: status
+	}
+	Results = append(Results, result)
+}
+
+func PrintResults(){
+	for _, res := range Results{
+		fmt.Printf("\n[%s] %s - $s\n",res.Status,res.Module,res.Target)
+		for k, v := range res.Results{
+			fmt.Printf(" - %s: %s\n",k,v)
+		}
+	}
+}
+
+func SaveReports(filename string) error{
+	file, err := os.Create(filename)
+	if err != nil{
+		return err
+	}
+	defer file.Close()
+
+	enc := json.NewEncoder(file)
+	enc.SetIndent(""," ")
+	return enc.Encode(Results)
+}
+
+
---
+++ utils/http.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func CleanURL(raw string) string{
+	if !strings.HasPrefix(raw,"http://") && strings.HasPrefix(raw,"https://"){
+		raw = "http://" + raw
+	}
+	return raw
+}
+
+func getDomain(raw string) string{
+	u, err := url.Parse(raw)
+	if err != nil {
+		fmt.Println("[-] Invalid URL: ",err)
+		return ""
+	}
+	return u.Hostname()
+}
+
+func StripProtocol(raw string) string{
+	raw = strings.TrimPrefix(raw,"https://")
+	raw = strings.TrimPrefix(raw,"http://")
+	return strings.TrimRight(raw,"/")
+}
